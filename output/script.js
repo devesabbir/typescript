258cc7b60259c7504c0eeadd011e5a381fc9c3eb
devesabbir <heartedsabbirdhrubo@gmail.com>
2023-06-09 16:02:11 +0600

    TypeScript Enviroment

---
+++ output/script.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log('Hello world!');
+const countryName = 'Bangladesh';
+function print(output) {
+    console.log(output);
+}
+print(countryName);
+const age = 3;
+print(age);
---
+++ src/script.ts
@@ -0,0 +1,10 @@
+console.log('Hello world!')
+const countryName:string = 'Bangladesh'
+function print(output:any): void{
+  console.log(output)
+}  
+
+print(countryName)
+const age:number = 3
+print(age)
+
