c44b85151fec516d7376a5fdd9f7f23a01cd7645
devssabbir <devssabbir@gmail.com>
2025-12-26 22:12:07 +0600

    simple data types

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// simple data types
+// string, number, boolean, null, undefined, symbol
+var myString = "Hello, TypeScript!";
+var myNumber = 42;
+var myBoolean = true;
+var myNull = null;
+var pName = "John Doe";
+var age = 30;
+var isEmployed = true;
+var message = "My name is ".concat(pName, " and I am ").concat(age, " years old. Employed: ").concat(isEmployed);
+console.log(message);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// simple data types
+// string, number, boolean, null, undefined, symbol
+let myString: string = "Hello, TypeScript!";
+let myNumber: number = 42;
+let myBoolean: boolean = true;
+let myNull: null = null;
+
+let pName: string = "John Doe";
+let age: number = 30;
+
+let isEmployed: boolean = true;
+
+let message: string = `My name is ${pName} and I am ${age} years old. Employed: ${isEmployed}`;
+console.log(message);
