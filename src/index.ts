d4e456d61ec29492480b2bca60db8b6aa039c3d4
devssabbir <devssabbir@gmail.com>
2026-01-23 16:01:18 +0600

    `Updated User interface and functions to use required properties and partial types`

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,64 @@
 // TypeScript code demonstrating the use of optional and required properties in interfaces
+// interface User {
+//   id: number;
+//   name: string;
+//   email?: string;
+// }
+
+// function createUser(obj: User): User {
+//   return {
+//     id: obj.id ?? Date.now(),
+//     name: obj.name ?? "Anonymous",
+//     email: obj.email ?? "",
+//   };
+// }
+
+// const newUser = createUser({ id: 1, name: "John Doe" });
+
+// function createAdminUser(obj: Required<User>): User {
+//   return {
+//     id: obj.id,
+//     name: obj.name,
+//     email: obj.email,
+//   };
+// }
+
+// const admin = createAdminUser({
+//   id: 2,
+//   name: "Admin User",
+//   email: "admin@email.com",
+// });
+
+// console.log({
+//   user: newUser,
+//   admin: admin,
+// });
+
+// partial types is opposite of required types
 interface User {
   id: number;
   name: string;
-  email?: string;
+  email: string;
 }
 
-function createUser(obj: User): User {
+function updateUser(obj: User, updatedUser: Partial<User>): User {
   return {
-    id: obj.id ?? Date.now(),
-    name: obj.name ?? "Anonymous",
-    email: obj.email ?? "",
+    ...obj,
+    ...updatedUser,
   };
 }
 
-const newUser = createUser({ id: 1, name: "John Doe" });
+const user: User = {
+  id: 1,
+  name: "John Doe",
+  email: "user@email.com",
+};
 
-function createAdminUser(obj: Required<User>): User {
-  return {
-    id: obj.id,
-    name: obj.name,
-    email: obj.email,
-  };
-}
-
-const admin = createAdminUser({
-  id: 2,
-  name: "Admin User",
-  email: "admin@email.com",
+const upUser = updateUser(user, {
+  email: "newemail@gmail.com",
 });
 
 console.log({
-  user: newUser,
-  admin: admin,
+  oldUser: user,
+  updatedUser: upUser,
 });
